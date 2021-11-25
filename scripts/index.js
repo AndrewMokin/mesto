a9a992980c197c9f2bcbe22dd39d32b7063e39ba
Andrew Mokin <mokinandrew@gmail.com>
2021-11-26 01:36:01 +0300

    fix: исправить ошибки после 1-го ревью

--- scripts/index.js
+++ scripts/index.js
@@ -3,29 +3,26 @@ const openProfile = document.querySelector('.profile__editing');
 const popup = document.querySelector('.popup');
 const popupCloseButton = popup.querySelector('.popup__close');
 const saveProfile = popup.querySelector('.popup__button')
-let nameInput = popup.querySelector('.popup__text-form_name');
+const nameInput = popup.querySelector('.popup__text-form_name');
 let jobInput = popup.querySelector('.popup__text-form_job');
 let namePage = document.querySelector('.profile__name');
 let jobPage = document.querySelector('.profile__description');
 
-function open() {
-  popup.classList.add('popup__opened');
+function openPopup() {
+  popup.classList.add('popup_opened');
 }
 
-function close() {
-  popup.classList.remove('popup__opened');
+function closePopup() {
+  popup.classList.remove('popup_opened');
 }
-
-openProfile.addEventListener('click', open);
-popupCloseButton.addEventListener('click', close);
-
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   namePage.textContent = nameInput.value;
   jobPage.textContent = jobInput.value;
+  closePopup();
 }
 
 popup.addEventListener('submit',formSubmitHandler);
-saveProfile.addEventListener('click',close);
-
+openProfile.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
