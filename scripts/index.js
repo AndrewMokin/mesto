91e656b2d95b9173f7234c18dfd8ce0898d1426d
Andrew Mokin <mokinandrew@gmail.com>
2021-12-02 23:23:22 +0300

    feat: добавляем карточки через JS метод InsertAdjacentHTML

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,17 @@
 const initialCards = [
-  name:
-  link: ''
+  {name: 'Огни ночной Москвы',
+  link: './images/moscow.jpg'},
+  {name: 'Бангкок',
+  link: './images/bangkok.jpg'},
+  {name: 'Из России в Дубаи',
+  link: './images/dubai.jpg'},
+  {name: 'Гонконг, как Кин-Конг',
+  link: './images/hongkong.jpg'},
+  {name: 'Лондон, тот самый',
+  link: './images/london.jpg'},
+  {name: 'Сан-Франциско - Диско',
+  link: './images/san_francisco.jpg'},
+
 ]
 
 
@@ -12,6 +23,34 @@ const nameInput = popup.querySelector('.popup__text-form_name');
 const jobInput = popup.querySelector('.popup__text-form_job');
 let namePage = document.querySelector('.profile__name');
 let jobPage = document.querySelector('.profile__description');
+
+const getTemplateEl = document.querySelector('.template');
+
+const listContainerEl = document.querySelector('.places')
+
+function render() {
+  const html = initialCards.map(getItemHTML).join('')
+  listContainerEl.insertAdjacentHTML("afterbegin", html);
+}
+
+function getItemHTML(item) {
+  return `<li class="place">
+  <img src=${item.link} alt="Москва" class="place__image">
+  <div class="place__info">
+    <h2 class="place__title">${item.name}</h2>
+    <button type="submit" class="place__like"></button>
+  </div>
+</li>`
+}
+
+render();
+
+// function getItem(item) {
+//   const newItem = getTemplateEl.content.cloneNode(true);
+//   const src = newItem.querySelector('.place__image');
+//   src.classList.add('item.link')
+//   return newItem;
+// }
 
 function openPopup() {
   popup.classList.add('popup_opened');
