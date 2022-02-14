14ba88eb308cd9319494b4a0284428a33541e19c
Andrew Mokin <mokinandrew@gmail.com>
2022-02-14 10:23:33 +0300

    рефакторинг перед webpack

--- utils/constants.js
+++ utils/constants.js
@@ -1,27 +1,27 @@
 export const initialCards = [
   {
     name: 'Огни ночной Москвы',
-    link: './images/moscow.jpg'
+    link: '../images/moscow.jpg'
   },
   {
     name: 'Бангкок',
-    link: './images/bangkok.jpg'
+    link: '../images/bangkok.jpg'
   },
   {
     name: 'Из России в Дубаи',
-    link: './images/dubai.jpg'
+    link: '../images/dubai.jpg'
   },
   {
     name: 'Гонконг, как Кин-Конг',
-    link: './images/hongkong.jpg'
+    link: '../images/hongkong.jpg'
   },
   {
     name: 'Лондон, тот самый',
-    link: './images/london.jpg'
+    link: '../images/london.jpg'
   },
   {
     name: 'Сан-Франциско - Диско',
-    link: './images/san_francisco.jpg'
+    link: '../images/san_francisco.jpg'
   },
 ]
 
